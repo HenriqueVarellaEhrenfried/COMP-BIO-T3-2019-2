0a513a74a0b6653753cc348929f5134b79abad16
Henrique V. Ehrenfried <h.v.ehrenfried@gmail.com>
2019-10-30 17:48:55 -0300

    Starting to use the BoW from the dataset

--- src/CreateGraph.py
+++ src/CreateGraph.py
@@ -2,10 +2,6 @@ import glob
 import math 
 import networkx as nx
 import matplotlib.pyplot as plt
-import spacy
-
-# Import english tokenizer
-nlp = spacy.load('en_core_web_sm')
 
 # Create Graph
 G = nx.Graph()
@@ -14,39 +10,63 @@ G = nx.Graph()
 with open("../dataset/imdb.vocab") as f:
     vocab = [line.rstrip() for line in f]
 
-vocab_dict = { i : {} for i in vocab}
+vocab_dict = { i : {"Total":0} for i in vocab}
 
-# Get all file names
-file_names_neg = glob.glob('../dataset/neg/*.txt')
-file_names_pos = glob.glob('../dataset/pos/*.txt')
-file_names = file_names_pos + file_names_neg
+# Read BoW
+with open("../dataset/labeledBow.feat") as f:
+    raw_bag_of_words = [line.rstrip() for line in f]
+
+bag_of_words = []
+review_names = []
+index = 0
+for rbow in raw_bag_of_words:
+    aux = rbow.split(" ")
+    bag_of_words.append(aux)
+    review_names.append(str(index) + '_' + str(aux[0]))
+    # NOTICE: aux[0] is the score the movie received
+    # The following for will create a total for each word
+    index = (index + 1) % 12500
+    for i in range(1,len(aux)):
+        splited = aux[i].split(":")
+        vocab_dict[vocab[int(splited[0])]]["Total"] = vocab_dict[vocab[int(splited[0])]]["Total"] + int(splited[1])
+    
+print(review_names)
+"""
+At this point I have:
+    * All words;
+    * All file names;
+    * All occurencies from each word
+    * All occurencies from each word in each file
+"""
 
 # Create a node for each vocabulary
 G.add_nodes_from(vocab)
 
-# Create a node for each file name
-G.add_nodes_from(file_names)
+# Create a node for each review name
+G.add_nodes_from(review_names)
 
 
 # Create edges (i,i) with weight 1 for each node i
-for node in vocab + file_names:
+for node in (vocab + review_names):
     G.add_edge(node, node, weight=1.0)
 
-
 """
-So far I created the graph, added all words and all files in it and 
-added the self edge (i,i)
+At this point I have:
+    * Nodes
+    * All self edges (i,i)
 """
 
-# Open each file and get each word and create its metrics
+
 
 
 
 # If you want to verify something use the above lines
 # print("Number of NODES >> ", G.number_of_nodes())
 # print("Number of EDGES >> ", G.number_of_edges())
-# print("Number of DOCS >> ", len(file_names))
+# print("Number of DOCS >> ", len(review_names))
 # print("Number of VOCAB >> ", len(vocab))
+
+# print(vocab_dict)
 
 
 # If you desire the Graviz version of the graph use the above line
