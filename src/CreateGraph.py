59418941ecc3131ecc81c7e71138aaad7327eab5
Henrique V. Ehrenfried <h.v.ehrenfried@gmail.com>
2019-10-31 17:16:05 -0300

    Started to implement edges between nodes

--- src/CreateGraph.py
+++ src/CreateGraph.py
@@ -4,7 +4,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # Create Graph
-G = nx.Graph()
+G = nx.MultiGraph()
 
 # Read vocabulary
 with open("../dataset/imdb.vocab") as f:
@@ -30,7 +30,6 @@ for rbow in raw_bag_of_words:
         splited = aux[i].split(":")
         vocab_dict[vocab[int(splited[0])]]["Total"] = vocab_dict[vocab[int(splited[0])]]["Total"] + int(splited[1])
     
-print(review_names)
 """
 At this point I have:
     * All words;
@@ -55,19 +54,32 @@ At this point I have:
     * Nodes
     * All self edges (i,i)
 """
-
+# The above code does not work as expected. It is needed to be examined
+for i in range(0, len(review_names)):
+    for j in range (1, len(bag_of_words[i])):
+        word = vocab[int(bag_of_words[i][j].split(":")[0])]
+        G.add_edge(word,review_names[i])
+        # Add edges between words in the same file
+        if j > 1:
+            for k in range(j-1, 0):
+                previous_word = vocab[int(bag_of_words[i][k].split(":")[0])]
+                G.add_edge(word, previous_word)                
 
 
 
 
 # If you want to verify something use the above lines
-# print("Number of NODES >> ", G.number_of_nodes())
-# print("Number of EDGES >> ", G.number_of_edges())
-# print("Number of DOCS >> ", len(review_names))
-# print("Number of VOCAB >> ", len(vocab))
+print("Number of NODES >> ", G.number_of_nodes())
+print("Number of EDGES >> ", G.number_of_edges())
+print("Number of DOCS >> ", len(review_names))
+print("Number of VOCAB >> ", len(vocab))
 
-# print(vocab_dict)
+# Before code from lines 63 to 66
+# Number of NODES >>  114527
+# Number of EDGES >>  3571212
+# Number of DOCS >>  25000
+# Number of VOCAB >>  89527
 
 
 # If you desire the Graviz version of the graph use the above line
-# nx.drawing.nx_agraph.write_dot(G,'./Dot/Test.dot')
+nx.drawing.nx_agraph.write_dot(G,'./Dot/Test.dot')
